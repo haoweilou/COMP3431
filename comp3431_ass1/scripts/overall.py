1db00bee962158d3f1d531742a670b597f7a84d1
haoweilou <591193117@qq.com>
2020-10-10 08:35:41 -0700

    navigation

---
+++ comp3431_ass1/scripts/overall.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import tf
+import json
+import rospy
+import roslaunch
+import actionlib
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+#initialise launch file and get initial pose & position
+def exploreStart():
+    rospy.init_node('en_Mapping', anonymous=True,disable_signals=True)
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+    explore = ['comp3431_ass1', 'explore.launch']
+    roslaunch_explore =roslaunch.rlutil.resolve_launch_arguments(explore)
+    launch = roslaunch.parent.ROSLaunchParent(uuid,roslaunch_explore)
+    launch.start()
+    rospy.loginfo("Start Explore")
+    #get initial postion
+    listener = tf.TransformListener()
+    rate = rospy.Rate(10.0)
+    now = rospy.Time.now()
+    #get current position
+    listener.waitForTransform("/odom", "/map", rospy.Time(0), rospy.Duration(3))
+    position, orientation = listener.lookupTransform('/odom', '/map', rospy.Time(0))
+    return position
+
+#send start message so robot can run
+def startRun():
+    rate = rospy.Rate(10)
+    rospy.loginfo("Send start")
+    for i in range(10):
+        rate.sleep()
+        pub = rospy.Publisher('cmd', String, queue_size=10)
+        rospy.loginfo('start')
+        pub.publish('start')
+
+#start navigation
+def navigation():
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+    navigation = ['comp3431_ass1', 'navigation.launch']
+    roslaunch_navigation =roslaunch.rlutil.resolve_launch_arguments(navigation)
+    launch = roslaunch.parent.ROSLaunchParent(uuid,roslaunch_navigation)
+    launch.start()
+    rospy.loginfo("Start Explore")
+
+def backToInitialGoal(position):
+    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    client.wait_for_server()
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = position[0]
+    goal.target_pose.pose.position.y = position[1]
+    goal.target_pose.pose.position.z = position[2]
+
+    goal.target_pose.pose.orientation.w = 1.0
+    client.send_goal(goal)
+    client.wait_for_result()
+
+if __name__ == "__main__":
+    initial_pose = exploreStart()
+    navigation()
+    startRun()
+    while True:
+        print("Running")
+    except KeyboardInterrupt:
+        backToInitialGoal(initial_pose)
--- comp3431_ass1/scripts/run.py
+++ comp3431_ass1/scripts/run.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
+import tf
+import json
 import rospy
 import roslaunch
 import actionlib
-import os
+from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
-import tf
-import json
-import sys
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+def callback(data):
+    return data.data
+    
 def Run():
     print("CALLED THE INITIALISATION FUNCTION")
     publishers = rospy.Publisher('s', PoseStamped, queue_size=1000)
+    #init node
     rospy.init_node('turtle_tf_listener')
     listener = tf.TransformListener()
     rate = rospy.Rate(10.0)
     now = rospy.Time.now()
+    #get current position
     listener.waitForTransform("/odom", "/map", now, rospy.Duration(3))
     position, orientation = listener.lookupTransform('/odom', '/map', rospy.Time(0))
-    print(position, orientation)
-    
+    #print(position, orientation)
+    #check goal
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
     print(1)
@@ -29,22 +33,19 @@ def Run():
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
     goal.target_pose.pose.position.x = position[0]
-    goal.target_pose.pose.position.y = position[1]
+    goal.target_pose.pose.position.y = 2
     goal.target_pose.pose.position.z = position[2]
 
     goal.target_pose.pose.orientation.w = 1.0
-    
+    command = rospy.Subscriber("/cmd", String, callback)
+    print(command.callback())
 
-    client.send_goal(goal)        
-    while not rospy.is_shutdown():
-        #rospy.loginfo(initialposition)
-        #publishers.publish(initialposition)
-        #print(initialposition)
-        rate.sleep()
-        break
+    end = input()
+    print("Finish")
+    client.send_goal(goal)
+    client.wait_for_result()
     print('run stop')
-#
-# rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped  '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: -0.209999740124, y: 1.78999996185, z: 0.0}, orientation: {w: 1.0}}}'
+    
 if __name__ == '__main__':
     print("start")
     try:
